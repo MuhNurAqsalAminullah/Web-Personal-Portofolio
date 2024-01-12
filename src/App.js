51d9a80ae76e10349bf566e364177fac8a4d68ad
MuhNurAqsalAminullah <aqsal3236@gmail.com>
2024-01-12 16:05:23 +0800

    move link

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const redirectTimeout = setTimeout(() => {
+      window.location.href = "https://muhnuraqsalaminullah-v2.vercel.app/"
+    }, 500)
+
+    return () => clearTimeout(redirectTimeout)
+  }, [])
   return (
     <div>
       <Home />
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
